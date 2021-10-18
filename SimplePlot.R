a9eb9ddd5dfcae73c8946fcd48ac1ae7c1158c8c
Joseph Kilgore <joey.a.kilgore@gmail.com>
2021-10-17 21:05:09 -0400

    update simple plot

--- SimplePlot.R
+++ SimplePlot.R
@@ -2,6 +2,7 @@
 covid<- read.csv(file ="owid-covid-data.csv", TRUE)
 head(covid)
 library(ggplot2)
+library(viridis)
 
 USA <- covid[as.Date(covid$date, origin="2019-12-31")<=as.Date(curDate, origin = "2019-12-31"),]
 USA <- USA[USA$location=="United States",]
@@ -9,6 +10,11 @@ head(USA)
 
 plot <- ggplot(data=USA)+
           geom_point(aes(x=as.Date(date),y=new_cases,size=new_tests,color=new_deaths))+
-          scale_color_gradientn(colours=rev(rainbow(7)))+
-          scale_size(range= c(1,10))
+          scale_color_viridis() +
+          scale_size(range= c(1,10))+
+          ggtitle("United States")+
+          xlab("Date")+
+          ylab("New Cases (Daily)")+
+          guides(size=guide_legend(title="New Tests (Daily)"), color=guide_legend(title="New Deaths (Daily)"))
 show(plot)
+ggsave("./US_Simple.png",plot, width=16, height=9)
